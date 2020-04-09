a1a7460cbfd37c27481dfd7216deec95865c2c80
Philip Chase <pbc@ufl.edu>
2020-04-08 21:25:52 -0400

    Create dataframe for swab_result ready for upload

--- fr_etl.R
+++ fr_etl.R
@@ -2,22 +2,37 @@ library(tidyverse)
 library(dotenv)
 library(REDCapR)
 
-# NOTE: For production script the data will be read from prod version of pid 8236 
+# NOTE: For production script the data will be read from pid 8270
 # to get the research_encounter_id which will then be written to pid 8258
   
 # set the timezone
 Sys.setenv(TZ = Sys.getenv("TIME_ZONE"))
 
-# read data from pid 8218
-records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
-                                       token = Sys.getenv("READ_FROM"))$data %>% 
-  filter(!is.na(research_encounter_id)) %>% 
-  select(research_encounter_id, covid_19_swab_result, igg_antibodies, igm_antibodies) %>% 
-  rename(record_id  = research_encounter_id) %>% 
-  mutate(igg_antibodies = c(rep("positive", 18), rep("negative", 14)),
-         igm_antibodies = c(rep("positive", 14), rep("negative", 18)))
+# read data from survey project
+survey_project_read <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                               token = Sys.getenv("WRITE_TO"))$data %>%
+  filter(!is.na(research_encounter_id)) %>%
+  select(record_id, redcap_event_name, research_encounter_id, 
+         covid_19_swab_result, igg_antibodies, igm_antibodies)
 
-# TODO:write data to pid 8236
+survey_swab_data <- survey_project_read %>%
+  filter(is.na(covid_19_swab_result)) %>%
+  select(record_id, redcap_event_name, research_encounter_id)
+
+# read data from result upload project, pid 8258
+upload_project_read <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                               token = Sys.getenv("READ_FROM"))$data
+
+# make result upload file for swabs
+swab_result <- upload_project_read %>%
+  filter(!is.na(record_id)) %>% 
+  select(research_encounter_id = record_id, covid_19_swab_result) %>%
+  filter(!is.na(covid_19_swab_result)) %>% 
+  inner_join(survey_swab_data, by=c("research_encounter_id")) %>% 
+  select(record_id, redcap_event_name, covid_19_swab_result) %>%
+  arrange(record_id) 
+
+# TODO:write data to survey project
 # redcap_write_oneshot(records, 
 #                      redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
 #                      token = Sys.getenv("WRITE_TO"))
