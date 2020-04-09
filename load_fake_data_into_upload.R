324764f63fbb4231c1120e6c39443089e0fa515f
Philip Chase <pbc@ufl.edu>
2020-04-09 10:46:45 -0400

    Change token vars, make random results, and write data in load_fake_data_into_upload.R

--- load_fake_data_into_upload.R
+++ load_fake_data_into_upload.R
@@ -7,18 +7,19 @@ library(REDCapR)
   
 # set the timezone
 Sys.setenv(TZ = Sys.getenv("TIME_ZONE"))
+# Sys.getenv("INSTANCE")
 
 # read data from pid 8218
 records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
-                                       token = Sys.getenv("READ_FROM"))$data %>% 
+                                       token = Sys.getenv("SURVEY_TOKEN"))$data %>% 
   filter(!is.na(research_encounter_id)) %>% 
   select(research_encounter_id, covid_19_swab_result, igg_antibodies, igm_antibodies) %>% 
   rename(record_id  = research_encounter_id) %>% 
-  mutate(igg_antibodies = c(rep("positive", 18), rep("negative", 14)),
-         igm_antibodies = c(rep("positive", 14), rep("negative", 18)))
+  mutate(covid_19_swab_result =  sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T)) %>%
+  mutate(igg_antibodies = sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T),
+         igm_antibodies = sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T))
 
 # TODO:write data to pid 8236
-# redcap_write_oneshot(records, 
-#                      redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
-#                      token = Sys.getenv("WRITE_TO"))
-
+redcap_write_oneshot(records, 
+                    redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                    token = Sys.getenv("RESULT_TOKEN"))
