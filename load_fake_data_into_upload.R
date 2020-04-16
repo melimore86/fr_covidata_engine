9e664140a90e3f2edc6b1bdec2fe40b0d2cf6d58
Philip Chase <pbc@ufl.edu>
2020-04-16 16:13:55 -0400

    Update load_fake_data_into_upload.R
    Add safety by testing the INSTANCE name.
    Make the script robust for any size of input dataset.

--- load_fake_data_into_upload.R
+++ load_fake_data_into_upload.R
@@ -2,24 +2,30 @@ library(tidyverse)
 library(dotenv)
 library(REDCapR)
 
-# NOTE: For production script the data will be read from prod version of pid 8236 
-# to get the research_encounter_id which will then be written to pid 8258
-  
 # set the timezone
 Sys.setenv(TZ = Sys.getenv("TIME_ZONE"))
-# Sys.getenv("INSTANCE")
+Sys.getenv("INSTANCE")
 
-# read data from pid 8218
 records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
                                        token = Sys.getenv("SURVEY_TOKEN"))$data %>% 
   filter(!is.na(research_encounter_id)) %>% 
-  select(research_encounter_id, covid_19_swab_result, igg_antibodies, igm_antibodies) %>% 
-  rename(record_id  = research_encounter_id) %>% 
-  mutate(covid_19_swab_result =  sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T)) %>%
-  mutate(igg_antibodies = sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T),
-         igm_antibodies = sample(c("Positive", "Negative", "negative", "positive"), 32, replace = T))
+  select(research_encounter_id, covid_19_swab_result, igg_antibodies, igm_antibodies)
 
-# TODO:write data to pid 8236
-redcap_write_oneshot(records, 
-                    redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
-                    token = Sys.getenv("RESULT_TOKEN"))
+# note the number of records we have so we can make samples of the same size
+n <- nrow(records)
+
+# make some fake test results
+results <- records %>%
+  rename(record_id  = research_encounter_id) %>%
+  mutate(covid_19_swab_result =  sample(c("Positive", "Negative", "negative", "positive"), n, replace = T)) %>%
+  mutate(igg_antibodies = sample(c("Positive", "Negative", "negative", "positive"), n, replace = T),
+         igm_antibodies = sample(c("Positive", "Negative", "negative", "positive"), n, replace = T))
+
+# Write data into the results project if it's safe to do so
+if (Sys.getenv("INSTANCE") == "Development") {
+  redcap_write_oneshot(results, 
+                       redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                       token = Sys.getenv("RESULT_TOKEN"))
+}
+
+
